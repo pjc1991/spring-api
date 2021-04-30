c991ba777adbe3e49f3d88d1ff232c34b39ef80a
pjc1991 <whdekf@naver.com>
2021-04-30 20:54:18 +0900

    Remove typo

--- src/main/java/com/misterymuscle/springapisample/controller/ApiController.java
+++ src/main/java/com/misterymuscle/springapisample/controller/ApiController.java
@@ -1,6 +1,5 @@
 package com.misterymuscle.springapisample.controller;
 
-\
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
