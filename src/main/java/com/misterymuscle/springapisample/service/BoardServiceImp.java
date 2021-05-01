8c47c067d8591b94918c4192a98ea0055278c514
pjc1991 <whdekf@naver.com>
2021-05-01 22:11:47 +0900

    Insert Service Implemented

--- src/main/java/com/misterymuscle/springapisample/service/BoardServiceImp.java
+++ src/main/java/com/misterymuscle/springapisample/service/BoardServiceImp.java
@@ -1,5 +1,6 @@
 package com.misterymuscle.springapisample.service;
 
+import com.misterymuscle.springapisample.domain.BoardArticle;
 import com.misterymuscle.springapisample.dto.BoardArticleDto;
 import com.misterymuscle.springapisample.repository.BoardRepository;
 
@@ -10,13 +11,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImp implements BoardService {
-    
+
     private final BoardRepository boardRepository;
 
     @Override
     public BoardArticleDto insertBoardArticle(BoardArticleDto dto) {
-        // TODO Auto-generated method stub
-        return null;
+        // dto to entity
+
+        BoardArticle article = BoardArticle.builder()
+                .boardTitle(dto.getBoardTitle())
+                .boardContent(dto.getBoardContent())
+                .memberId(dto.getMemberId())
+                .build();
+        // there is no member function yet
+
+        article = boardRepository.save(article);
+        // entity to dto;
+        BoardArticleDto rtn = BoardArticleDto.builder()
+                .boardIdx(article.getBoardIdx())
+                .boardTitle(article.getBoardTitle())
+                .boardContent(article.getBoardContent())
+                .boardRegDate(article.getBoardRegDate())
+                .boardModDate(article.getBoardModDate())
+                .memberId(article.getMemberId())
+                .build();
+
+        return rtn;
     }
-    
+
 }
