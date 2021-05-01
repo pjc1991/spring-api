dfbbd5e38aebd06a59e7bea8394577b2920e57b9
pjc1991 <whdekf@naver.com>
2021-05-01 22:11:02 +0900

    ApiController Mapping

--- src/main/java/com/misterymuscle/springapisample/controller/ApiController.java
+++ src/main/java/com/misterymuscle/springapisample/controller/ApiController.java
@@ -6,6 +6,7 @@ import com.misterymuscle.springapisample.service.BoardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class ApiController {
 
     private final BoardService boardService;
