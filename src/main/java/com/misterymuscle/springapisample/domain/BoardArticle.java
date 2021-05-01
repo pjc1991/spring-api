db57845d78c342d2ae69f856f3780153e3a09bae
pjc1991 <whdekf@naver.com>
2021-05-01 22:11:21 +0900

    RegDate, ModDate

--- src/main/java/com/misterymuscle/springapisample/domain/BoardArticle.java
+++ src/main/java/com/misterymuscle/springapisample/domain/BoardArticle.java
@@ -6,6 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,10 +30,29 @@ public class BoardArticle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long boardIdx;
+
     private String boardTitle;
+
     private String boardContent;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private Date boardRegDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date boardModDate;
+
     private String memberId;
+
+    @PrePersist
+    void preInsert(){
+        if(this.boardRegDate == null){
+            this.boardRegDate = new Date();
+        }
+        if(this.boardModDate == null){
+            this.boardModDate = new Date();
+        }
+    }
 
     
 }
--- src/main/java/com/misterymuscle/springapisample/dto/BoardArticleDto.java
+++ src/main/java/com/misterymuscle/springapisample/dto/BoardArticleDto.java
@@ -2,15 +2,18 @@ package com.misterymuscle.springapisample.dto;
 
 import java.util.Date;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class BoardArticleDto {
 
     private long boardIdx;
     private String boardTitle;
     private String boardContent;
     private Date boardRegDate;
+    private Date boardModDate;
     private String memberId;
     
 }
