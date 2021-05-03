4820c0bcdfc15c6cbfdebf5a13d4b5ae667bdf9a
pjc1991 <whdekf@naver.com>
2021-05-03 22:18:43 +0900

    selectBoardArticle Service

--- src/main/java/com/misterymuscle/springapisample/service/BoardServiceImp.java
+++ src/main/java/com/misterymuscle/springapisample/service/BoardServiceImp.java
@@ -1,7 +1,11 @@
 package com.misterymuscle.springapisample.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.misterymuscle.springapisample.domain.BoardArticle;
 import com.misterymuscle.springapisample.dto.BoardArticleDto;
+import com.misterymuscle.springapisample.dto.BoardArticleSearch;
 import com.misterymuscle.springapisample.repository.BoardRepository;
 
 import org.springframework.stereotype.Service;
@@ -17,7 +21,6 @@ public class BoardServiceImp implements BoardService {
     @Override
     public BoardArticleDto insertBoardArticle(BoardArticleDto dto) {
         // dto to entity
-
         BoardArticle article = BoardArticle.builder()
                 .boardTitle(dto.getBoardTitle())
                 .boardContent(dto.getBoardContent())
@@ -37,6 +40,22 @@ public class BoardServiceImp implements BoardService {
                 .build();
 
         return rtn;
+    }
+
+    @Override
+    public List<BoardArticleDto> selectBoardArticles(BoardArticleSearch search) {
+        List<BoardArticle> list = boardRepository.findAll();
+        List<BoardArticleDto> dtoList = list.stream()
+                                        .map(article -> BoardArticleDto.builder()
+                                        .boardIdx(article.getBoardIdx())
+                                        .boardTitle(article.getBoardTitle())
+                                        .boardContent(article.getBoardContent())
+                                        .boardRegDate(article.getBoardRegDate())
+                                        .boardModDate(article.getBoardModDate())
+                                        .build())
+                                        .collect(Collectors.toList());
+
+        return dtoList;
     }
 
 }
